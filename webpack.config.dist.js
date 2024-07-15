18d5c2ad14d0335476ba231f6f4cb858fddb63ca
Konstantine Tsafatinos <kosta.tsaf@gmail.com>
2024-07-15 19:22:35 -0400

    update webpack and imgs path

--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -8,7 +8,7 @@ module.exports = {
   ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'OrcidLogin.jsx',
+    filename: 'OrcidLogin.js',
     libraryTarget: 'umd',
     library: 'OrcidLogin'
   },
